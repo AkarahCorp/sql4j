70bcd88b2c711b460bc8cd68d2b32d2fc4e3575e
akarahdev <endisticbutitsforgit@gmail.com>
2025-05-21 21:21:09 -0400

    feat: add Database class

---
+++ src/main/java/net/akarah/sql4j/Database.java
@@ -0,0 +1,77 @@
+package net.akarah.sql4j;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    Connection connection;
+
+    String databaseName;
+    String hostname;
+
+    String username;
+    String password;
+
+    private Database() {}
+
+    public static Database create(String databaseName) {
+        var db = new Database();
+        db.databaseName = databaseName;
+        return db;
+    }
+
+    public Database hostname(String hostname) {
+        this.hostname = hostname;
+        return this;
+    }
+
+    public Database username(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public Database password(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public Database loadProperties(Database.Properties properties) {
+        this.hostname = properties.hostname();
+        this.username = properties.username();
+        this.password = properties.password();
+        return this;
+    }
+
+    public Connection connection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection("jdbc:postgresql://" + this.hostname + "/" + this.databaseName, username, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public record Properties(String hostname, String username, String password) {
+        public static Database.Properties loadFromFile(String fileName) {
+            var props = new java.util.Properties();
+            try {
+                props.load(new FileInputStream(fileName));
+            } catch (IOException e) {
+                // this exception should not happen unless smth is horribly wrong
+                throw new RuntimeException(e);
+            }
+
+            var username = (String) props.remove("user");
+            var password = (String) props.remove("password");
+            var hostname = (String) props.remove("hostname");
+
+            return new Database.Properties(hostname, username, password);
+        }
+    }
+}
--- src/test/java/TestHelpers.java
+++ src/test/java/TestHelpers.java
@@ -1,32 +1,18 @@
+import net.akarah.sql4j.Database;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
 
 public class TestHelpers {
+    public static Database DATABASE = Database.create("testing_database")
+            .loadProperties(Database.Properties.loadFromFile("./.env"));
+
     public static Connection localDbConnection() {
-        try {
-            Class.forName("org.postgresql.Driver");
-
-            var props = new Properties();
-            props.load(new FileInputStream("./.env"));
-
-            var username = (String) props.remove("user");
-            var password = (String) props.remove("password");
-            var url = (String) props.remove("url");
-
-            return DriverManager.getConnection(url, username, password);
-        } catch (ClassNotFoundException | SQLException | IOException e) {
-            throw new RuntimeException(e);
-        }
+        return DATABASE.connection();
     }
 
     @Test
-    public void testConnection() throws SQLException {
-        TestHelpers.localDbConnection();
+    public void testConnection() {
+        var conn = localDbConnection();
     }
 }
